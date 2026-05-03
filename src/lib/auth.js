a2a52fe76ff3e6bd546ffec865c94736475b38e3
Galib Mehboob <86629449+Galibmehboob@users.noreply.github.com>
2026-05-03 17:03:10 +0600

    fixing errors

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,8 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import dns from "dns";
-dns.setDefaultResultOrder("ipv4first")
+
+dns.setDefaultResultOrder("ipv4first");
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
@@ -10,7 +11,14 @@ await client.connect();
 
 const db = client.db();
 
+const BETTER_AUTH_URL =
+    process.env.BETTER_AUTH_URL || "http://localhost:3000";
+
 export const auth = betterAuth({
+    baseURL: BETTER_AUTH_URL,
+
+    trustedOrigins: [BETTER_AUTH_URL],
+
     emailAndPassword: {
         enabled: true,
     },
